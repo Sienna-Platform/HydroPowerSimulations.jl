7c27a4575987c8f84e8f0a22ee80a36e3d0b62c7
Luke Kiernan <lkiernan@nlr.gov>
2026-07-20 09:40:15 -0600

    offer curves breakpoints: units conversion bug

--- src/hydro_generation.jl
+++ src/hydro_generation.jl
@@ -192,14 +192,11 @@ PSI.get_multiplier_value(::InflowTimeSeriesParameter, d::PSY.HydroReservoir, ::H
 PSI.get_multiplier_value(::InflowTimeSeriesParameter, d::PSY.HydroReservoir, ::HydroWaterFactorModel) = PSY.get_inflow(d)
 PSI.get_multiplier_value(::PSI.TimeSeriesParameter, d::PSY.HydroGen, ::AbstractHydroFormulation) = PSY.get_max_active_power(d)
 PSI.get_multiplier_value(::PSI.TimeSeriesParameter, d::PSY.HydroGen, ::PSI.FixedOutput) = PSY.get_max_active_power(d)
-# Time series values are in component base, where 1.0 corresponds to `get_max_active_power`,
-# so the min-active-power series multiplies by `get_max_active_power` (NOT `get_min_active_power`).
-# This is covered by the generic `TimeSeriesParameter` fallback above, but is stated explicitly
-# to document the convention and prevent a well-intentioned switch to `get_min_active_power`.
+# Time series values are in component base. Note: purposefully NOT `get_min_active_power`.
 PSI.get_multiplier_value(::PSI.MinActivePowerTimeSeriesParameter, d::PSY.HydroGen, ::AbstractHydroFormulation) = PSY.get_max_active_power(d)
-# next 2 needed to avoid ambiguity errors
-PSI.get_multiplier_value(::PSI.AbstractPiecewiseLinearBreakpointParameter, d::PSY.HydroGen, ::PSI.FixedOutput) = PSY.get_max_active_power(d)
-PSI.get_multiplier_value(::PSI.AbstractPiecewiseLinearBreakpointParameter, d::PSY.HydroGen, ::AbstractHydroFormulation) = PSY.get_max_active_power(d)
+# next 2 needed to avoid ambiguity errors. Note: purposefully NOT `get_max_active_power`.
+PSI.get_multiplier_value(::PSI.AbstractPiecewiseLinearBreakpointParameter, d::PSY.HydroGen, ::PSI.FixedOutput) = 1.0
+PSI.get_multiplier_value(::PSI.AbstractPiecewiseLinearBreakpointParameter, d::PSY.HydroGen, ::AbstractHydroFormulation) = 1.0
 
 PSI.get_parameter_multiplier(::ReservoirTargetParameter, ::PSY.HydroReservoir, ::HydroWaterModelReservoir) = 1.0
 PSI.get_parameter_multiplier(::ReservoirTargetParameter, ::PSY.HydroReservoir, ::HydroEnergyModelReservoir) = 1.0
